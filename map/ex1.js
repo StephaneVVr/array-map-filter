53bdc35f3c60227d15c752930e097f8312bc8270
Stéphane Vivier <contact.xsprod@gmail.com>
2019-03-12 17:43:45 +0100

    Ajout exo1 map

--- map/ex1.js
+++ map/ex1.js
@@ -22,6 +22,7 @@ En sortie: [
  */
 
 function getStringsLength(strings) {
+  return strings.map(el => `${el} contains ${el.length} characters`);
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
